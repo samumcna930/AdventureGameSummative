e055a2ef807f8a2766542ed9660de9cae6d7b700
samumcna930 <samumcna930@SNS-D-130S0W>
2019-10-29 12:55:59 -0400

    Add project files.

---
+++ AdventureGameSummative/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace AdventureGameSummative
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gameTitle = new System.Windows.Forms.Label();
+            this.playButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // gameTitle
+            // 
+            this.gameTitle.AutoSize = true;
+            this.gameTitle.BackColor = System.Drawing.Color.Transparent;
+            this.gameTitle.Font = new System.Drawing.Font("Rockwell", 72F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gameTitle.Location = new System.Drawing.Point(-3, 220);
+            this.gameTitle.Name = "gameTitle";
+            this.gameTitle.Size = new System.Drawing.Size(470, 120);
+            this.gameTitle.TabIndex = 0;
+            this.gameTitle.Text = "The Hunt";
+            // 
+            // playButton
+            // 
+            this.playButton.BackColor = System.Drawing.Color.Transparent;
+            this.playButton.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.playButton.FlatAppearance.BorderSize = 0;
+            this.playButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Red;
+            this.playButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Red;
+            this.playButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.playButton.Font = new System.Drawing.Font("Schneidler BT", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.playButton.Location = new System.Drawing.Point(31, 343);
+            this.playButton.Name = "playButton";
+            this.playButton.Size = new System.Drawing.Size(202, 67);
+            this.playButton.TabIndex = 1;
+            this.playButton.Text = "Hunt";
+            this.playButton.UseVisualStyleBackColor = false;
+            this.playButton.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::AdventureGameSummative.Properties.Resources.TheHuntImage;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(764, 467);
+            this.Controls.Add(this.playButton);
+            this.Controls.Add(this.gameTitle);
+            this.Name = "Form1";
+            this.Text = "TheHuntGame";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label gameTitle;
+        private System.Windows.Forms.Button playButton;
+    }
+}
+
