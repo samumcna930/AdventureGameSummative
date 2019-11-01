35f40c4d6c6714201c2cf53e7c33ba8e227e65e9
samumcna930 <samumcna930@SNS-D-130S0W>
2019-11-01 13:24:39 -0400

    Finshed picture boxes

--- AdventureGameSummative/Form1.Designer.cs
+++ AdventureGameSummative/Form1.Designer.cs
@@ -31,6 +31,17 @@
             this.gameTitle = new System.Windows.Forms.Label();
             this.playButton = new System.Windows.Forms.Button();
             this.outputLabel = new System.Windows.Forms.Label();
+            this.pictureBoxM = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBoxB = new System.Windows.Forms.PictureBox();
+            this.pictureBoxN = new System.Windows.Forms.PictureBox();
+            this.optionLabel1 = new System.Windows.Forms.Label();
+            this.optionLabel2 = new System.Windows.Forms.Label();
+            this.optionLabel3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxM)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxB)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxN)).BeginInit();
             this.SuspendLayout();
             // 
             // gameTitle
@@ -71,6 +82,65 @@
             this.outputLabel.Size = new System.Drawing.Size(583, 136);
             this.outputLabel.TabIndex = 2;
             // 
+            // pictureBoxM
+            // 
+            this.pictureBoxM.Location = new System.Drawing.Point(198, 220);
+            this.pictureBoxM.Name = "pictureBoxM";
+            this.pictureBoxM.Size = new System.Drawing.Size(48, 43);
+            this.pictureBoxM.TabIndex = 3;
+            this.pictureBoxM.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(-23, -46);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBoxB
+            // 
+            this.pictureBoxB.Location = new System.Drawing.Point(198, 283);
+            this.pictureBoxB.Name = "pictureBoxB";
+            this.pictureBoxB.Size = new System.Drawing.Size(48, 46);
+            this.pictureBoxB.TabIndex = 5;
+            this.pictureBoxB.TabStop = false;
+            // 
+            // pictureBoxN
+            // 
+            this.pictureBoxN.Location = new System.Drawing.Point(198, 343);
+            this.pictureBoxN.Name = "pictureBoxN";
+            this.pictureBoxN.Size = new System.Drawing.Size(48, 46);
+            this.pictureBoxN.TabIndex = 6;
+            this.pictureBoxN.TabStop = false;
+            // 
+            // optionLabel1
+            // 
+            this.optionLabel1.BackColor = System.Drawing.Color.Transparent;
+            this.optionLabel1.Font = new System.Drawing.Font("Schadow Lt BT", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.optionLabel1.Location = new System.Drawing.Point(269, 213);
+            this.optionLabel1.Name = "optionLabel1";
+            this.optionLabel1.Size = new System.Drawing.Size(426, 50);
+            this.optionLabel1.TabIndex = 7;
+            // 
+            // optionLabel2
+            // 
+            this.optionLabel2.BackColor = System.Drawing.Color.Transparent;
+            this.optionLabel2.Font = new System.Drawing.Font("Schadow Lt BT", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.optionLabel2.Location = new System.Drawing.Point(273, 283);
+            this.optionLabel2.Name = "optionLabel2";
+            this.optionLabel2.Size = new System.Drawing.Size(422, 46);
+            this.optionLabel2.TabIndex = 8;
+            // 
+            // optionLabel3
+            // 
+            this.optionLabel3.BackColor = System.Drawing.Color.Transparent;
+            this.optionLabel3.Font = new System.Drawing.Font("Schadow Lt BT", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.optionLabel3.Location = new System.Drawing.Point(274, 343);
+            this.optionLabel3.Name = "optionLabel3";
+            this.optionLabel3.Size = new System.Drawing.Size(421, 46);
+            this.optionLabel3.TabIndex = 9;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -78,12 +148,23 @@
             this.BackgroundImage = global::AdventureGameSummative.Properties.Resources.TheHuntImage;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(764, 467);
+            this.Controls.Add(this.pictureBoxN);
+            this.Controls.Add(this.pictureBoxB);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pictureBoxM);
             this.Controls.Add(this.outputLabel);
             this.Controls.Add(this.playButton);
             this.Controls.Add(this.gameTitle);
+            this.Controls.Add(this.optionLabel2);
+            this.Controls.Add(this.optionLabel3);
+            this.Controls.Add(this.optionLabel1);
             this.Name = "Form1";
             this.Text = "TheHuntGame";
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxM)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxB)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxN)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -94,6 +175,13 @@
         private System.Windows.Forms.Label gameTitle;
         private System.Windows.Forms.Button playButton;
         private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.PictureBox pictureBoxM;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBoxB;
+        private System.Windows.Forms.PictureBox pictureBoxN;
+        private System.Windows.Forms.Label optionLabel1;
+        private System.Windows.Forms.Label optionLabel2;
+        private System.Windows.Forms.Label optionLabel3;
     }
 }
 
--- AdventureGameSummative/Form1.cs
+++ AdventureGameSummative/Form1.cs
@@ -12,39 +12,59 @@ using System.Threading;
 namespace AdventureGameSummative
 {
     public partial class Form1 : Form
-    { 
+    {
+        bool hasRifle = false;
         bool hasFishing = false;
         bool hasKey = false;
         int key = 0;
         int scene = 0;
         int bulletChance = 35;
+        int scene35Rng;
         public Form1()
         {
             InitializeComponent();
+            pictureBoxB.Visible = false;
+            pictureBoxM.Visible = false;
+            pictureBoxN.Visible = false;
+            Random randGen = new Random();
 
         }
 
         private void Button1_Click(object sender, EventArgs e)
         {
             gameTitle.Text = "";
-
+            Thread.Sleep(800);
+            playButton.Visible = false;
+            outputLabel.Text = "It is winter, there is a man eating grizzly that is lurking about in the winter wood." +
+                      "You have to hunt it and kill it. However there are supplies around that may help your hunt.";
+            optionLabel1.Text = "Abandoned cabin";
+            optionLabel2.Text = "Along riverbed";
+            optionLabel3.Text = "Head North";
+            pictureBoxB.Visible = true;
+            pictureBoxM.Visible = true;
+            pictureBoxN.Visible = true;
         }
 
         private void Form1_KeyDown(object sender, KeyEventArgs e)
         {
             if (e.KeyCode == Keys.M)       //red button press
             {
-                if (scene == 0) { scene = 1; }
-                else if (scene == 1) { scene = 2; }
+                if (scene == 1) { scene = 2; }
                 else if (scene == 2) { scene = 3; }
-                else if (scene == 3) { scene = 4; }
+                else if (scene == 3) { scene = 4; hasFishing = true; }
                 else if (scene == 4) { scene = 0; }
                 else if (scene == 5) { scene = 0; }
-                else if (scene == 35) { }
-               
+                else if (scene == 35)
                 {
-                    if (hasKey) { scene = 35; }
+                    if (hasKey)
+                    {
+                        Random randGen = new Random();
+                        scene35Rng = randGen.Next(1, 11);
+                        if (scene35Rng == 1) { scene = 7; }
+                        else { scene = 6; }
+                    }
 
+                    else { scene = 4; }
                 }
 
                 else if (scene == 9)
@@ -64,12 +84,13 @@ namespace AdventureGameSummative
 
             else if (e.KeyCode == Keys.B)
             {
-                if (scene == 0) { scene = 9; }
-                else if (scene == 1) { scene = 2; }
+                if (scene == 1) { scene = 9; }
                 else if (scene == 2) { scene = 3; }
                 else if (scene == 3) { scene = 1; }
                 else if (scene == 4) { scene = 5; }
                 else if (scene == 5) { scene = 4; }
+
+                else if (scene == 35) { scene = 6; }
 
 
                 else if (scene == 9) { scene = 10;}
@@ -81,33 +102,36 @@ namespace AdventureGameSummative
 
            else if (e.KeyCode == Keys.N)
             {
-            if (scene == 0) { scene = 14; }
+            if (scene == 1) { scene = 14; }
             }
-           switch (scene)
 
+
+            switch (scene)
             {
-                case 0: //start Game
+                case 0: 
                     break;
                 case 1:
                     outputLabel.Text = "It is winter, there is a man eating grizzly that is lurking about in the winter wood." +
-                        "You have to hunt it and kill it. However there are supplies around that may help your hunt.";
-                    redlabel.Text = "Abandoned cabin";
-                    bluelabel.Text = "Along riverbed";
+                        "You have to hunt it and kill it. However there are supplies around that may help your hunt. + " +
+                        "Head North when you are ready to challenge the grizzly";
+                    optionLabel1.Text = "Abandoned cabin";
+                    optionLabel2.Text = "Along riverbed";
+                    optionLabel3.Text = "Head North";
                     break;
                 case 2:
                     outputLabel.Text = "There is no food to be found here, however you find a fishing rod.";
-                    redlabel.Text = "Take the rod";
-                    bluelabel.Text = "Leave it";
+                    optionLabel1.Text = "Take the rod";
+                    optionLabel2.Text = "Leave it";
                     break;
                 case 3:
                     outputLabel.Text = "There are more things to explore in the cabin.";
-                    redlabel.Text = "Keep searching";
-                    bluelabel.Text = "Don't search";
+                    optionLabel1.Text = "Keep searching";
+                    optionLabel2.Text = "Don't search";
                     break;
                 case 4:
                     outputLabel.Text = "You find 3 rifle bullets";
-                    redlabel.Text = "Keep searching";
-                    bluelabel.Text = "Go back to hut";
+                    optionLabel1.Text = "Keep searching";
+                    optionLabel2.Text = "Go back to hut";
                     break;
                 case 5:
                     outputLabel.Text = "You find a locked chest.";
@@ -116,8 +140,7 @@ namespace AdventureGameSummative
                     break;
                 case 6:
                     outputLabel.Text = "You find a hunting rifle inside the chest";
-                    redlabel.Text = "";
-                    bluelabel.Text = "";
+                    hasRifle = true;
                     break;
                 case 7: outputLabel.Text = "The key has broken";
                     break;
@@ -125,46 +148,46 @@ namespace AdventureGameSummative
                     outputLabel.Text = "With nothing left to find at the cabin you return to your hut";
                     break;
                 case 9: outputLabel.Text = "You come along to the river shore and can see many trout in swimming with the current.";
-                    redlabel.Text = "catch the fishes";
-                    bluelabel.Text = "keep going";
+                    optionLabel1.Text = "catch the fishes";
+                    optionLabel2.Text = "keep going";
                     break;
                 case 10:
                     outputLabel.Text = "You come across a cold dead body of a man. It looks he was killed by the grizzly.";
-                    redlabel.Text = "loot the body";
-                    bluelabel.Text = "Don't loot the body";
+                    optionLabel1.Text = "loot the body";
+                    optionLabel2.Text = "Don't loot the body";
                     break;
                 case 11:
                     outputLabel.Text = "You loot the dead man and find a rusty key.You also find a hunting knife.";
-                    redlabel.Text = "go back to riverbed";
-                    bluelabel.Text = "go back to hut";
+                    optionLabel1.Text = "go back to riverbed";
+                    optionLabel2.Text = "go back to hut";
                     break;
                 case 12:
                     outputLabel.Text = "You are successful in catching some trout. ";
-                    redlabel.Text = "Go back to hut";
-                    bluelabel.Text = "Don't go back to the hut";
+                    optionLabel1.Text = "Go back to hut";
+                    optionLabel2.Text = "Don't go back to the hut";
                     break;
                 case 13:
                     outputLabel.Text = " You try to catch fish with your bare hands however the water gives you hypothermia and you freeze to death  ";
                     break;
                 case 14:
                     outputLabel.Text = "You are entering the grizzly's territory";
-                    redlabel.Text = "fight it";
-                    bluelabel.Text = "retreat";
+                    optionLabel1.Text = "fight it";
+                    optionLabel2.Text = "retreat";
                     break;
                 case 15:
                     outputLabel.Text = "You are confident with your resources in hand that you may overcome the grizzly. ";
-                    redlabel.Text = "beat the bear with hands";
-                    bluelabel.Text = "Snipe the bear";
+                    optionLabel1.Text = "beat the bear with hands";
+                    optionLabel2.Text = "Snipe the bear";
                     break;
                 case 16:
                     outputLabel.Text = "The grizzly is coming! ";
-                    redlabel.Text = "Fight";
-                    bluelabel.Text = "Flight";
+                    optionLabel1.Text = "Fight";
+                    optionLabel2.Text = "Flight";
                     break;
                 case 17:
                     outputLabel.Text = "You wait for the grizzly in cover for many hours. Finally you spot the beast.";
-                    redlabel.Text = "Fight";
-                    bluelabel.Text = "Flight";
+                    optionLabel1.Text = "Fight";
+                    optionLabel2.Text = "Flight";
                     break;
                 case 18:
                     outputLabel.Text = "The grizzly sees you running and catches up and murders you";
@@ -174,13 +197,13 @@ namespace AdventureGameSummative
                     break;
                 case 20:
                     outputLabel.Text = "The bullet misses, and the bear charges at you";
-                    redlabel.Text = "shoot again";
-                    bluelabel.Text = "Climb a tree";
+                    optionLabel1.Text = "shoot again";
+                    optionLabel2.Text = "Climb a tree";
                     break;
                 case 21:
                     outputLabel.Text = "The bear is closing in on you";
-                    redlabel.Text = "Take another shot";
-                    bluelabel.Text = "Run";
+                    optionLabel1.Text = "Take another shot";
+                    optionLabel2.Text = "Run";
                     break;
                 case 22:
                     outputLabel.Text = "You try to outrun the grizzly however it catches up to you and kills you";
@@ -190,8 +213,9 @@ namespace AdventureGameSummative
                     break;
                 case 24:
                     outputLabel.Text = "You missed";
-                    redlabel.Text = "Take another shot";
-                    bluelabel.Text = "Run";
+                    optionLabel1.Text = "Take another shot";
+                    optionLabel2.Text = "Run";
+                    break;
                 case 25:
                     outputLabel.Text = "You ran out of bullets. The grizzly kills you.";
                     break;
@@ -203,18 +227,18 @@ namespace AdventureGameSummative
                     break;
                 case 28:
                     outputLabel.Text = "You climb up a tree and the bear is close behind. ";
-                    redlabel.Text = "Shoot";
-                    bluelabel.Text = "Throw fish";
-                    greenlabel.Text = "Climb higher";
+                    optionLabel1.Text = "Shoot";
+                    optionLabel2.Text = "Throw fish";
+                    optionLabel3.Text = "Climb higher";
                     break;
                 case 29:
                     outputLabel.Text = "You shoot the grizzly in the face at point blank range.";
                     break;
                 case 30:
-                    outputLabel.Text = "Your hands slip and you fall to your death.";
+                    outputLabel.Text = "Your hands slip and you fall to your death.";                   
                     break;
                 case 31:
-                    outputLabel.Text = "the grizzly whilst climbing the tree tries to catch the fish midair, slips and falls to it's death.";
+                    outputLabel.Text = "The grizzly whilst climbing the tree tries to catch the fish midair, slips and falls to it's death.";
                     break;
                 case 32:
                     outputLabel.Text = "The grizzly is quick to catch up, and effortlessly eats you up.";
@@ -227,26 +251,26 @@ namespace AdventureGameSummative
                     break;
                 case 35:
                     outputLabel.Text = "The key matches the chest.";
-                    redlabel.Text = "Use the rusty key";
-                    bluelabel.Text = "Return to hut";
-
+                    optionLabel1.Text = "Use the rusty key";
+                    optionLabel2.Text = "Return to hut";
+                    break;
                 case 96:
                     outputLabel.Text = "You have died and unsuccessfully killed the bear.";
                     break;
                 case 97:
                      outputLabel.Text = "Would you like to try again?";
-                     redlabel.Text = "Yes";
-                     bluelabel.Text = "No";
-                        break;
+                     optionLabel1.Text = "Yes";
+                     optionLabel2.Text = "No";
+                    break;
                 case 98:
                      outputLabel.Text = "You have died and unsuccessfully killed the bear.";
-                        break;
+                    break;
                  case 99:
                      outputLabel.Text = "Would you like to try again?";
-                     redlabel.Text = "Yes";
-                     bluelabel.Text = "No";
-                        break;
-
+                     optionLabel1.Text = "Yes";
+                     optionLabel2.Text = "No";
+                    break;
+                    
 
 
 
